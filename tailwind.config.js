e2c28bd13f1d24a42388c9744cc10fef6eaa5763
jhunter5 <jhunter@unal.edu.co>
2024-05-18 20:25:39 -0500

    added dependecy for colors m3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const { withMaterialColors } = require('tailwind-material-colors');
+
+const config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -15,4 +17,17 @@ module.exports = {
     },
   },
   plugins: [],
-};
+}
+
+module.exports = withMaterialColors(config, {
+  // Your base colors as HEX values. 'primary' is required.
+  primary: '#006A61',
+  // secondary and/or tertiary are optional, if not set they will be derived from the primary color.
+  secondary: '#231942',
+  tertiary: '#F8B1DC',
+  },
+  {
+  /* one of 'content', 'expressive', 'fidelity', 'monochrome', 'neutral', 'tonalSpot' or 'vibrant' */
+  scheme: 'content',
+  contrast: 0,
+  });
